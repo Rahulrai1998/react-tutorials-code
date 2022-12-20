18d2e2ea41e71264646d5ffbff24509afc23a78c
Rahul Kumar <rahulrai2822march@gmail.com>
2022-12-21 00:03:27 +0530

    updated

--- CWHarryReact/text-utils/src/components/TextForm.js
+++ CWHarryReact/text-utils/src/components/TextForm.js
@@ -3,6 +3,7 @@ let btnText = 0;
 export default function TextForm(props) {
   const [set, setState] = useState(0);
   const [text, setText] = useState("Enter and Click Button");
+
   //   const handleChange=()=> {
   //     setState(set + 1);
   //     // btnText+=1;
@@ -10,33 +11,53 @@ export default function TextForm(props) {
   //   const testText = "freeeeee"
   //   }
   return (
-    <div className="form-group">
-      <label htmlFor="myBox">{props.formHeading}</label>
-      <textarea
-        className="form-control"
-        id="myBox"
-        onChange={(e) => {
-        //   console.log(e.target.value);
-          setText(e.target.value);
-        }}
-        rows="6"
-        value={text}
-      ></textarea>
-      <button
-        className="btn btn-primary my-2"
-        onClick={() => {
-        //   setText(text);
-        //   setState(set + 1);
-        //   btnText += 1;
-        // console.log(text)
-        setText(text.toUpperCase())
+    <>
+      <div className="container">
+        <label htmlFor="myBox">{props.formHeading}</label>
 
-        }}
-      >
-        {/* {set}
+        <textarea
+          className="form-control"
+          id="myBox"
+          onChange={(e) => {
+            //   console.log(e.target.value);
+            setText(e.target.value);
+          }}
+          rows="6"
+          value={text}
+        ></textarea>
+        <button
+          className="btn btn-primary my-2"
+          onClick={() => {
+            //   setText(text);
+            //   setState(set + 1);
+            //   btnText += 1;
+            // console.log(text)
+            setText(text.toUpperCase());
+          }}
+        >
+          {/* {set}
           {btnText} */}
-        HIT ME HARDER
-      </button>
-    </div>
+          HIT ME HARDER
+        </button>
+        <button
+          className="btn btn-secondary mx-3 my-2"
+          onClick={() => {
+            setText("");
+          }}
+        >
+          Reset
+        </button>
+      </div>
+      <div className="container my-3">
+        <h3>Word Counter</h3>
+        <p>
+          {text.split(" ").length} words and {text.length} characters{" "}
+        </p>
+        <p>{0.008 * text.split(" ").length} minutes</p>
+
+        <h3>Preview</h3>
+        <p>{text}</p>
+      </div>
+    </>
   );
 }
